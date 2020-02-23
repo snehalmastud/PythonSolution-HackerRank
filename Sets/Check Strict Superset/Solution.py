12cd48ae4f698cb890afffb0838b4e36c66cef06
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-23 09:26:22 +0530

    Python Sets!

---
+++ Sets/Check Strict Superset/Solution.py
@@ -0,0 +1,13 @@
+A = set(map(int, raw_input().split()))
+N = int(raw_input())
+
+isstrictsuperset = True
+
+for i in range(N):
+    s = set(map(int, raw_input().split()))
+    if not s.issubset(A):
+        isstrictsuperset = False
+    if len(s) >= len(A):
+        isstrictsuperset = False
+
+print isstrictsuperset
