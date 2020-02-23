18f1359b66f336d764e7251f00619e0f8de20b2f
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-23 10:26:32 +0530

    Python Collections!

---
+++ Collections/Collections.namedtyple()/Solution.py
@@ -0,0 +1,14 @@
+from __future__ import division
+import collections
+
+N = int(raw_input())
+columns = ','.join(raw_input().split())
+Student = collections.namedtuple('Student', columns)
+
+sum = 0
+for i in range(N):
+    line = raw_input().split()
+    student = Student(*line)
+    sum += int(student.MARKS)
+
+print sum / N
