1141ac01ee1649ca7ded892cd56c8f7056e2a463
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-23 10:13:04 +0530

    Math!

---
+++ Math/Find Angle MBC/Solution.py
@@ -0,0 +1,14 @@
+from math import sqrt
+from math import acos
+from math import degrees
+
+AB = int(input())
+BC = int(input())
+
+AC = sqrt(AB ** 2 + BC ** 2)
+
+BM = AC / 2
+
+angle = acos(BC / (2*BM))
+
+print("{}{}".format(round(degrees(angle)), chr(176)))
