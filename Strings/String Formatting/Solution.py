65d4c51259ca8d6b27b77318ccbca4bed333c1df
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-23 09:09:09 +0530

    Strings in Python!

---
+++ Strings/String Formatting/Solution.py
@@ -0,0 +1,10 @@
+def print_formatted(number):
+     w = len(str(bin(number)).replace('0b',''))
+     for i in range(1,number+1):
+
+        d = str(i).rjust(w,' ')
+        b = bin(i)[2:].rjust(w,' ')   ## rjust is used for line alignment
+        o = oct(i)[2:].rjust(w, ' ')
+        h = hex(i)[2:].rjust(w, ' ').upper()
+        print(d, o, h, b)
+
