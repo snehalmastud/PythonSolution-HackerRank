7ac985a8e9348bebf1abc21736c763367c6485f9
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-23 11:50:15 +0530

    Delete Solution.py

--- Built-lns/Zipped!/Solution.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n, x = map(int, input().split())
-
-sheet = []
-for _ in range(x):
-    sheet.append(map(float, input().split()))
-
-for i in zip(*sheet):
-    print(sum(i) / len(i))
